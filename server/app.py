62846a78ee10d0030493d5b9bb05f5fe96839519
Rohan Mathur <rohanmathur34@gmail.com>
2015-08-06 20:17:13 -0400

    Various updates
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import abort, Flask, render_template
+from flask import abort, Flask, jsonify, render_template, request
 
 from models import db, Data
 
@@ -11,9 +11,38 @@ def show_user_profile(username):
     user = Data.query.filter_by(username=username).first()
 
     if user:
-        return render_template('profile.html', username=username)
+        return render_template('profile.html', username=user.username, battery_percent=user.battery_percent, status=user.status)
     else:
         abort(404)
+
+@app.route('/update')
+def update_user_profile():
+    username = request.args.get('username')
+    battery_percent = request.args.get('battery_percent')
+    status = request.args.get('status')
+
+    ret_json = {
+        "message": "",
+        "code": "",
+    }
+
+    # show the user profile for that user
+    user = Data.query.filter_by(username=username).first()
+
+    if user:
+        user.battery_percent = battery_percent
+        if status:
+            user.status = status
+        db.session.commit()
+        ret_json["message"] = "Successfully updated"
+        ret_json["code"] = 200
+    else:
+        user = Data(username, battery_percent, status)
+        db.session.add(user)
+        db.session.commit()
+        ret_json["message"] = "Successfully created"
+        ret_json["code"] = 200
+    return jsonify(ret_json)
 
 if __name__ == '__main__':
     db.init_app(app)
